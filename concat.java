2dde1cec602330c273cb64a2e2fb58222ad4a67d
SSG <suvojit.sengupta.ssg@gmail.com>
2023-08-31 14:15:11 +0530

    adding concat

---
+++ concat.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+
+    // create first string
+    String first = "Java";
+    System.out.println("First String: " + first);
+
+    // create second
+    String second = "Programming";
+    System.out.println("Second String: " + second);
+
+    // join two strings with concat function 
+    String joinedString = first.concat(second);
+    System.out.println("Joined String with concat : " + joinedString);
+    // join two strings with + operator 
+    System.out.println(first+second);
+    System.out.println("Joined String with (+) operator: " + joinedString);
+  }
+}
